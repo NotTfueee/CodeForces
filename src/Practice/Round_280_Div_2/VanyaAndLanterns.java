04c0a68229ce6461284555dd436b092b39891b63
Anurag <100501102+NotTfueee@users.noreply.github.com>
2024-01-19 14:46:53 +0530

    solved

--- src/Practice/Round_280_Div_2/VanyaAndLanterns.java
+++ src/Practice/Round_280_Div_2/VanyaAndLanterns.java
@@ -1,126 +1,50 @@
 package Practice.Round_280_Div_2;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
+//File Created by -- > anuragbhatt
+//Created On -- > 19/01/24,Friday
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class VanyaAndLanterns {
-    static class Reader {
-        final private int BUFFER_SIZE = 1 << 16;
-        private DataInputStream din;
-        private byte[] buffer;
-        private int bufferPointer, bytesRead;
 
-        public Reader() {
-            din = new DataInputStream(System.in);
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        public Reader(String file_name) throws IOException {
-            din = new DataInputStream(new FileInputStream(file_name));
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public String readLine() throws IOException {
-            byte[] buf = new byte[64]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1) {
-                if (c == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-                buf[cnt++] = (byte) c;
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
             }
-            return new String(buf, 0, cnt);
+            return st.nextToken();
         }
 
-        public int nextInt() throws IOException {
-            int ret = 0;
-            byte c = read();
-            while (c <= ' ') {
-                c = read();
-            }
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (neg)
-                return -ret;
-            return ret;
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
         }
 
-        public long nextLong() throws IOException {
-            long ret = 0;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-            if (neg)
-                return -ret;
-            return ret;
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
         }
 
-        public double nextDouble() throws IOException {
-            double ret = 0, div = 1;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (c == '.') {
-                while ((c = read()) >= '0' && c <= '9') {
-                    ret += (c - '0') / (div *= 10);
-                }
-            }
-
-            if (neg)
-                return -ret;
-            return ret;
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
         }
 
-        private void fillBuffer() throws IOException {
-            bytesRead = din.read(buffer, bufferPointer = 0,
-                    BUFFER_SIZE);
-            if (bytesRead == -1)
-                buffer[0] = -1;
-        }
-
-        private byte read() throws IOException {
-            if (bufferPointer == bytesRead)
-                fillBuffer();
-            return buffer[bufferPointer++];
-        }
-
-        public void close() throws IOException {
-            if (din == null)
-                return;
-            din.close();
+        String nextLine() throws IOException {
+            return br.readLine();
         }
     }
 
     public static void main(String[] args) throws IOException {
-        var sc = new Reader();
-
+        var sc = new FastReader();
         int n = sc.nextInt() ;
         long l = sc.nextLong();
 
