ea378d440892f7d0a6698f35b0510475bf50c2ca
Anurag <100501102+NotTfueee@users.noreply.github.com>
2024-02-11 22:11:04 +0530

    Div 4

---
+++ src/Contests/Feb_11_2024/Round_924_Div_2/q1.java
@@ -0,0 +1,60 @@
+package Contests.Feb_11_2024.Round_924_Div_2;
+
+//File Created by -- > anuragbhatt
+//Created On -- > 11/02/24,Sunday
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q1 {
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        var sc = new FastReader();
+        int t = sc.nextInt();
+
+        while(t-- > 0)
+        {
+            long a = sc.nextLong(), b = sc.nextLong();
+
+            if ((a % 2 != 0 && b % 2 != 0) || (a % 2 != 0 && b == a * 2) || (b % 2 != 0 && a == b * 2)) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+    }
+}
